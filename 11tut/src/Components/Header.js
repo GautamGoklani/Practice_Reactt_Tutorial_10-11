d38b624a9d927def6c3ce8cf96dc63a7c98e2091
GautamGoklani <94471716+GautamGoklani@users.noreply.github.com>
2024-06-17 22:25:25 +0530

    Initial Commit

---
+++ 11tut/src/Components/Header.js
@@ -0,0 +1,15 @@
+const Header = ({ title }) => {
+
+  // const headerStyle = {
+  //   backgroundColor: 'mediumblue',
+  //   color: '#fff'
+  // }
+
+  return (
+    <header>
+        <h1>{title}</h1>
+    </header>
+  )
+}
+
+export default Header
